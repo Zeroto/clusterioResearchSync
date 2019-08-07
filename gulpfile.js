db895e24039e544f1f27e2e282f3c89e48753e1e
Sander Homan <sander.homan@wordtango.net>
2019-08-07 19:41:19 +0200

    Make sure that it uses the locally installed tsc and not global

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const exec = require('child_process').exec;
 
 gulp.task('compile', function (done) {
-  exec('tsc', function (err, stdOut, stdErr) {
+  exec('npm run compile', function (err, stdOut, stdErr) {
     console.log(stdOut);
     if (err){
       done(err);
